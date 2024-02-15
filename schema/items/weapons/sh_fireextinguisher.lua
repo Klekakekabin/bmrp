d441a1d38facdf71b7f55f88681aed1be3d0c562
Klekakekabin <159907308+Klekakekabin@users.noreply.github.com>
2024-02-15 16:30:57 +0000

    Plugins/business

--- schema/items/weapons/sh_fireextinguisher.lua
+++ schema/items/weapons/sh_fireextinguisher.lua
@@ -3,8 +3,8 @@ ITEM.description = "A fire extinguisher used to control fires in the case of an 
 ITEM.model = "models/weapons/w_fireextinguisher1.mdl"
 ITEM.class = "weapon_fextinguisher"
 ITEM.weaponCategory = "utility"
-ITEM.width = 2
-ITEM.height = 3
+ITEM.width = 1
+ITEM.height = 2
 ITEM.weight = 2.5
 ITEM.noBusiness = true
 
--- schema/items/weapons/sh_milflashlight.lua
+++ schema/items/weapons/sh_milflashlight.lua
@@ -1,10 +1,10 @@
 ITEM.name = "HECU Flashlight"
-ITEM.description = "A flashlight used to illuminate dark areas by the United States Marine Corps."
+ITEM.description = "A tactical flashlight used to illuminate dark areas by the United States Marine Corps."
 ITEM.model = "models/props_marines/mil_flashlight.mdl"
 ITEM.class = "weapon_bm_flashlight_milit"
 ITEM.weaponCategory = "utility"
 ITEM.width = 1
-ITEM.height = 
+ITEM.height = 1
 ITEM.weight = 0.25
 ITEM.noBusiness = true
 
