b10d09fb3549d4881e41bd276c895fc2163233f4
Klekakekabin <159907308+Klekakekabin@users.noreply.github.com>
2024-02-18 07:49:50 +0000

    Added outfits & plugins

---
+++ schema/items/outfit/sh_hazmat.lua
@@ -0,0 +1,9 @@
+ITEM.name = "Hazmat Suit"
+ITEM.description = "An overall garment worn to protect people from hazardous materials or substances, including chemicals, biological agents, or radioactive materials."
+ITEM.category = "Outfit"
+ITEM.model = "models/weapons/w_suitcase_passenger.mdl"
+ITEM.width = 2 
+ITEM.height = 3
+ITEM.weight = 8.5
+
+ITEM.replacements = "models/humans_bmcer/pm/scientist_cleansuit.mdl"
