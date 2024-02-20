f6b7f9eeec1662888e9c083147830783d990f705
Klekakekabin <159907308+Klekakekabin@users.noreply.github.com>
2024-02-20 23:59:25 +0000

    headcrab meat

---
+++ schema/items/consumables/sh_food_headcrab.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Headcrab Meat"
+ITEM.model = "models/Gibs/HGIBS_scapula.mdl"
+ITEM.description = "A chunk of meat cut from a headcrab."
+ITEM.permit = "consumables"
+ITEM.hunger = 20
+ITEM.thirst = 0
+ITEM.noBusiness = true
